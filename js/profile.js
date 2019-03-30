53cfd3031e7c922ddaa7953261f20fd58312cdcc
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-03-30 18:33:50 +0200

    added update functionality to name and zipcode field

--- js/profile.js
+++ js/profile.js
@@ -3,31 +3,43 @@ const userInfo = document.querySelector('.user-info');
 let nameSpan = document.querySelector('#full-name');
 let birthdaySpan = document.querySelector('#birthday');
 let zipSpan = document.querySelector('#zip-code');
+let submitButton = document.querySelector('.submit-button');
 
 // creating function to show user data 
-
 function renderUser (user){
 
-    nameSpan.placeholder = user.data().firstName + ' ' + user.data().lastName;
+    nameSpan.placeholder = user.data().name;
     birthdaySpan.placeholder = '01/02/1990';
     zipSpan.placeholder = user.data().zipCode;
 }
 
+//getting data from the firebase
 db.collection('users').get().then((snapshot) =>{
     snapshot.docs.forEach(element => {
         renderUser(element);       
     })   
 })
 
-// function getBirthday(user){
-//     let dateObject = new Date(user.data().birthDay.seconds * 1000);
-//     let date = dateObject.getDate();
-//     let month = dateObject.getMonth() + 1;
-//     let year = dateObject.getFullYear();
-    
-//     if(date < 10) date = '0' + date;
-//     if(month < 10) month = '0' + month;
-    
-    
-//     return `${date}/${month}/${year}`;
-// }
+// updating data 
+submitButton.addEventListener('click', (e) =>{
+    e.preventDefault();
+    //check if not empty 
+    if(nameSpan.value != '' && zipSpan.value != '' ){
+        db.collection('users').doc('MSeiYZ0TeTyd5CG2WdfB').update({
+        
+            name: nameSpan.value,
+            zipCode: zipSpan.value
+        
+        }) 
+    }
+    // set fields to empty
+    nameSpan.value = '';
+    zipSpan.value = '';
+    // get values 
+    db.collection('users').get().then((snapshot) =>{
+        snapshot.docs.forEach(element => {
+            renderUser(element);       
+        })   
+    })
+
+})
